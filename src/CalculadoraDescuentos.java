fc99085561f6296a89a514f9fa99ed880a5d5692
JuanCarlosCarbajosaGarcia <juanc.cargar@educa.jcyl.es>
2025-11-14 10:34:27 +0100

    v1.0

--- src/CalculadoraDescuentos.java
+++ src/CalculadoraDescuentos.java
@@ -1,2 +1,7 @@
 public class CalculadoraDescuentos {
+    public static final double Descuento_Estudiante=0.20;
+    public static final double Descuento_Adulto=0.30;
+    public static final double Descuento_Mayor=0.30;
+    public static final double Descuento_VIP=0.15;
+    public static final double IVA=0.21;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    public static double aplicarDescuento(double precio, String Cliente){
+        Cliente=Cliente.toLowerCase();
+
+        switch(Cliente){
+            case "Estudiante":
+                return precio*(1-CalculadoraDescuentos.Descuento_Estudiante);
+            case "Adulto":
+                return precio*(1-CalculadoraDescuentos.Descuento_Adulto);
+            case "Mayor":
+                return precio*(1-CalculadoraDescuentos.Descuento_Mayor);
+            case "VIP":
+                return precio*(1-CalculadoraDescuentos.Descuento_VIP);
+
+            default:
+                return precio;
+        }
+    }
+    public static double aplicarIVA(double precioconDescuento){
+        return precioconDescuento*(1-CalculadoraDescuentos.IVA);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("ingrese precio base: ");
+        double preciobase=sc.nextDouble();
+        sc.nextLine();
+
+        System.out.println("ingrese tipo de cliente (Estudiante,Adulto,Mayor,VIP): ");
+        String Cliente=sc.nextLine();
+
+        double precioconDescuento=aplicarDescuento(preciobase,Cliente);
+
+        double IVA=aplicarIVA(precioconDescuento);
+
+        System.out.println("-------Resultado-------");
+        System.out.println("Precio con Descuento: " + String.format("%.2f",precioconDescuento));
+        System.out.println("IVA: " + String.format("%.2f",IVA));
+        sc.close();
     }
 }
