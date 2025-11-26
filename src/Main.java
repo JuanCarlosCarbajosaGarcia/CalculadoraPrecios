9e9d780e6a4fe8d3a077ec7cd510b533179bb1fe
JuanCarlosCarbajosaGarcia <juanc.cargar@educa.jcyl.es>
2025-11-26 13:09:11 +0100

    v1.1 arreglo al switch

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,22 @@ public class Main {
         Cliente=Cliente.toLowerCase();
 
         switch(Cliente){
+            case "1":
             case "Estudiante":
                 return precio*(1-CalculadoraDescuentos.Descuento_Estudiante);
+
+            case "2":
             case "Adulto":
                 return precio*(1-CalculadoraDescuentos.Descuento_Adulto);
+
+            case "3":
             case "Mayor":
                 return precio*(1-CalculadoraDescuentos.Descuento_Mayor);
+
+            case "4":
             case "VIP":
                 return precio*(1-CalculadoraDescuentos.Descuento_VIP);
+
 
             default:
                 return precio;
@@ -28,7 +36,7 @@ public class Main {
         double preciobase=sc.nextDouble();
         sc.nextLine();
 
-        System.out.println("ingrese tipo de cliente (Estudiante,Adulto,Mayor,VIP): ");
+        System.out.println("ingrese tipo de cliente (1:Estudiante,2:Adulto,3:Mayor,4:VIP): ");
         String Cliente=sc.nextLine();
 
         double precioconDescuento=aplicarDescuento(preciobase,Cliente);
@@ -36,7 +44,8 @@ public class Main {
         double IVA=aplicarIVA(precioconDescuento);
 
         System.out.println("-------Resultado-------");
-        System.out.println("Precio con Descuento: " + String.format("%.2f",precioconDescuento));
+        System.out.println("Precio base: " + String.format("%.2f",preciobase));
+        System.out.println("precio con descuento: " + String.format("%.2f",precioconDescuento));
         System.out.println("IVA: " + String.format("%.2f",IVA));
         sc.close();
     }
